4fa722c310711ea8d5d251e70101b2ef1099c3e2
Andrzej Tunkiel <53775802+AndrzejTunkiel@users.noreply.github.com>
2021-03-04 22:19:17 +0100

    Update resampling_test.py

--- resampling_test.py
+++ resampling_test.py
@@ -15,83 +15,83 @@ from sklearn.neighbors import KNeighborsRegressor
 #%%
 extensions = np.linspace(1,10,50)
 global_results = []
+carlo = 1000
+start = 0
+stop = 5
+samples = 100
+#%%
 for i in extensions:
     print(".", end="")
     local_results = []
-    for j in range(10):
-        x = np.random.uniform(0,10,200)
+    for j in range(carlo):
+        start = 0
+        stop = 10
+        samples = 100
+        x = np.random.uniform(start, stop, samples)
         x = np.sort(x)
         y = np.sin(x)
         
         
         max_step = np.max(np.diff(x))
         extension = i
-        
-        # plt.plot(y)
-        # plt.plot(x*20,y)
-        # plt.show()
+
         reg = RadiusNeighborsRegressor(radius=max_step*extension,
                                        weights='distance',
                                        algorithm = 'auto')
         reg.fit(x.reshape(-1,1),y)
         
-        x_uniform = np.linspace(0,10,100)
+        x_uniform = np.linspace(start, stop, samples)
         
         y_reg = reg.predict(x_uniform.reshape(-1,1))
         
-        # plt.plot(x_uniform, y_reg)
-        # plt.show()
+
         
-        diff = np.sin(x_uniform[10:-10])-y_reg[10:-10]
-        # plt.plot(diff)
+        diff = y[10:-10]-y_reg[10:-10]
         result = np.mean(np.abs(diff))
         
         local_results.append(result)
-        # print(f'Mean error is {np.mean(np.abs(diff))}')
+
         
     global_results.append(np.mean(local_results))
 
-plt.scatter(extensions, global_results, marker = '.')
+plt.scatter(extensions, global_results, marker = '.', label='rnn')
 plt.grid()
-#plt.ylim(0,0.008)
 
+#%%
 print()
-extensions = np.linspace(1,10,50)
+carlo=1000
+
+extensions = np.arange(1,101,1)
 global_results = []
 for i in extensions:
     print(",", end="")
     local_results = []
-    for j in range(10):
-        x = np.random.uniform(0,10,200)
+    for j in range(carlo):
+        x = np.random.uniform(start, stop, samples)
         x = np.sort(x)
         y = np.sin(x)
-        
         
         max_step = np.max(np.diff(x))
         extension = i
         
 
-        reg = KNeighborsRegressor(n_neighbors = 5,
+        reg = KNeighborsRegressor(n_neighbors = i,
                                   weights='distance')
         reg.fit(x.reshape(-1,1),y)
         
-        x_uniform = np.linspace(0,10,100)
+        x_uniform = np.linspace(start, stop , samples)
         
         y_reg = reg.predict(x_uniform.reshape(-1,1))
         
-        # plt.plot(x_uniform, y_reg)
-        # plt.scatter(x,y, alpha=0.6, c='red')
-        # plt.show()
         
-        diff = np.sin(x_uniform[10:-10])-y_reg[10:-10]
-        # plt.plot(diff)
+        diff = y[10:-10]-y_reg[10:-10]
+
         result = np.mean(np.abs(diff))
         
         local_results.append(result)
-        # print(f'Mean error is {np.mean(np.abs(diff))}')
+
         
     global_results.append(np.mean(local_results))
 
-plt.scatter(extensions, global_results, marker = '.')
-#plt.grid()
-#plt.ylim(0,0.008)
+plt.scatter(extensions, global_results, marker = '.', label = 'knn')
+

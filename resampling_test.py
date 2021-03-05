c8f456f463d62a8dc8a5a9196be9119d19bb0613
Andrzej Tadeusz Tunkiel <2921228@uis.no>
2021-03-05 10:31:26 +0100

    Update resampling_test.py

--- resampling_test.py
+++ resampling_test.py
@@ -15,7 +15,7 @@ from sklearn.neighbors import KNeighborsRegressor
 #%%
 extensions = np.linspace(1,10,50)
 global_results = []
-carlo = 1000
+carlo = 100
 start = 0
 stop = 5
 samples = 100
@@ -54,12 +54,14 @@ for i in extensions:
         
     global_results.append(np.mean(local_results))
 
-plt.scatter(extensions, global_results, marker = '.', label='rnn')
+plt.scatter(extensions, global_results, marker = '.', label='RNR', c='black')
+plt.xlabel('Radius multiplier')
+plt.ylabel('MAE')
 plt.grid()
 
 #%%
 print()
-carlo=1000
+carlo=100
 
 extensions = np.arange(1,101,1)
 global_results = []
@@ -93,5 +95,48 @@ for i in extensions:
         
     global_results.append(np.mean(local_results))
 
-plt.scatter(extensions, global_results, marker = '.', label = 'knn')
+plt.scatter(extensions, global_results, marker = '.', label = 'KNN', c='black')
+plt.xlabel('Neighbour count')
+plt.ylabel('MAE')
+plt.legend()
 
+#%%
+
+from statsmodels.nonparametric.smoothers_lowess import lowess
+
+
+
+carlo=100
+
+extensions = np.linspace(0.01,1,100)
+global_results = []
+for i in extensions:
+    print(",", end="")
+    local_results = []
+    for j in range(carlo):
+        x = np.random.uniform(start, stop, samples)
+        x = np.sort(x)
+        y = np.sin(x)
+        
+        max_step = np.max(np.diff(x))
+        extension = i
+        
+    
+        x_uniform = np.linspace(start, stop , samples)
+        
+        y_reg = lowess(y, x, frac=i, xvals=x_uniform)
+        
+        
+        diff = y[10:-10]-y_reg[10:-10]
+
+        result = np.mean(np.abs(diff))
+        
+        local_results.append(result)
+
+        
+    global_results.append(np.mean(local_results))
+
+plt.scatter(extensions, global_results, marker = '.', label = 'LOWESS', c='black')
+plt.xlabel('Data fraction')
+plt.ylabel('MAE')
+plt.legend()

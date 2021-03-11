73429e60b47f89bc8e7e7fa9498142ae8f40f5f2
Andrzej Tadeusz Tunkiel <2921228@uis.no>
2021-03-11 15:22:24 +0100

    upd

--- core_resampling_test.py
+++ core_resampling_test.py
@@ -17,12 +17,14 @@ extensions = np.linspace(1,10,100)
 
 start = 0
 stop = 5
-samples = 1000
+samples = 100
 
 sto=100
 
 carlo = 10*sto
-fig, axs = plt.subplots(3,2, figsize = (9,7), sharey=True)
+
+
+fig, axs = plt.subplots(2,2, figsize = (6,8), sharey=True)
 global_results = []
 global_p95 = []
 global_p5 = []
@@ -33,7 +35,7 @@ for i in extensions:
     for j in range(carlo):
         start = 0
         stop = 10
-        samples = 100
+
         x = np.random.uniform(start, stop, samples)
         x = np.sort(x)
         y = np.sin(x)
@@ -50,10 +52,10 @@ for i in extensions:
         x_uniform = np.linspace(start, stop, samples)
         
         y_reg = reg.predict(x_uniform.reshape(-1,1))
-        
+        y_uniform = np.sin(x_uniform)
 
         
-        diff = y[10:-10]-y_reg[10:-10]
+        diff = y_uniform[2:-2]-y_reg[2:-2]
         result = np.mean(np.abs(diff))
         
         local_results.append(result)
@@ -74,8 +76,14 @@ axs[0,0].scatter(extensions, global_p95, marker = '_', label='RNR',
 axs[0,0].set_xlabel('Radius multiplier')
 axs[0,0].set_ylabel('Mean Absolute Error')
 axs[0,0].grid()
+
 axs[0,0].set_axisbelow(True)
-axs[0,0].title.set_text('RadiusNeighborsRegressor, weights=distance')
+axs[0,0].title.set_text('RadiusNeighborsRegressor,\nweights=distance')
+axs[0,0].axhline(y=0.199, xmin=0, xmax=1, color='black', linewidth=1,
+                 label='no resampling,\naverage')
+axs[0,0].axhline(y=0.101, xmin=0, xmax=1, color='black', linewidth=1,
+                 label='no resampling,\n5th/95th percentile', linestyle = '--')
+axs[0,0].axhline(y=0.35, xmin=0, xmax=1, color='black', linewidth=1, linestyle = '--')
 
 print()
 
@@ -91,7 +99,7 @@ for i in extensions:
     for j in range(carlo):
         start = 0
         stop = 10
-        samples = 100
+
         x = np.random.uniform(start, stop, samples)
         x = np.sort(x)
         y = np.sin(x)
@@ -110,8 +118,8 @@ for i in extensions:
         y_reg = reg.predict(x_uniform.reshape(-1,1))
         
 
-        
-        diff = y[10:-10]-y_reg[10:-10]
+        y_uniform = np.sin(x_uniform)        
+        diff = y_uniform[2:-2]-y_reg[2:-2]
         result = np.mean(np.abs(diff))
         
         local_results.append(result)
@@ -133,14 +141,18 @@ axs[0,1].set_xlabel('Radius multiplier')
 
 axs[0,1].grid()
 axs[0,1].set_axisbelow(True)
-axs[0,1].title.set_text('RadiusNeighborsRegressor, weights=uniform')
-
+axs[0,1].title.set_text('RadiusNeighborsRegressor,\nweights=uniform')
+axs[0,1].axhline(y=0.199, xmin=0, xmax=1, color='black', linewidth=1,
+                 label='no resampling,\naverage')
+axs[0,1].axhline(y=0.101, xmin=0, xmax=1, color='black', linewidth=1,
+                 label='no resampling,\n5th/95th percentile', linestyle = '--')
+axs[0,1].axhline(y=0.35, xmin=0, xmax=1, color='black', linewidth=1, linestyle = '--')
 print()
 
 carlo=10*sto
 global_p95 = []
 global_p5 = []
-extensions = np.arange(1,101,1)
+extensions = np.arange(1,51,1)
 global_results = []
 for i in extensions:
     print(",", end="")
@@ -162,8 +174,8 @@ for i in extensions:
         
         y_reg = reg.predict(x_uniform.reshape(-1,1))
         
-        
-        diff = y[10:-10]-y_reg[10:-10]
+        y_uniform = np.sin(x_uniform)        
+        diff = y_uniform[2:-2]-y_reg[2:-2]
 
         result = np.mean(np.abs(diff))
         
@@ -175,27 +187,32 @@ for i in extensions:
     global_p95.append(np.percentile(local_results,95))
     
     
-axs[1,0].scatter(extensions, global_results, marker = '.', label='RNR',
+axs[1,0].scatter(extensions, global_results, marker = '.', label='mean',
                c='black', s=10)
 
-axs[1,0].scatter(extensions, global_p5, marker = '_', label='RNR',
+axs[1,0].scatter(extensions, global_p5, marker = '_', label='5th/95th percentile',
                c='gray', s=10)
 
-axs[1,0].scatter(extensions, global_p95, marker = '_', label='RNR',
+axs[1,0].scatter(extensions, global_p95, marker = '_',
                c='gray', s=10)
 axs[1,0].set_xlabel('Neighbour count')
 
 axs[1,0].grid()
 axs[1,0].set_ylabel('Mean Absolute Error')
 axs[1,0].set_axisbelow(True)
-axs[1,0].title.set_text('KNeighborsRegressor, weights=distance')
-
+axs[1,0].title.set_text('KNeighborsRegressor,\nweights=distance')
+axs[1,0].axhline(y=0.199, xmin=0, xmax=1, color='black', linewidth=1,
+                 label='no resampling,\naverage')
+axs[1,0].axhline(y=0.101, xmin=0, xmax=1, color='black', linewidth=1,
+                 label='no resampling,\n5th/95th percentile', linestyle = '--')
+axs[1,0].axhline(y=0.35, xmin=0, xmax=1, color='black', linewidth=1, linestyle = '--')
+axs[1,0].legend()
 print()
 
 carlo=10*sto
 global_p95 = []
 global_p5 = []
-extensions = np.arange(1,101,1)
+extensions = np.arange(1,51,1)
 global_results = []
 for i in extensions:
     print("^", end="")
@@ -217,8 +234,8 @@ for i in extensions:
         
         y_reg = reg.predict(x_uniform.reshape(-1,1))
         
-        
-        diff = y[10:-10]-y_reg[10:-10]
+        y_uniform = np.sin(x_uniform)        
+        diff = y_uniform[2:-2]-y_reg[2:-2]
 
         result = np.mean(np.abs(diff))
         
@@ -230,88 +247,94 @@ for i in extensions:
     global_p95.append(np.percentile(local_results,95))
     
     
-axs[1,1].scatter(extensions, global_results, marker = '.', label='RNR',
+axs[1,1].scatter(extensions, global_results, marker = '.', label='mean',
                c='black', s=10)
 
-axs[1,1].scatter(extensions, global_p5, marker = '_', label='RNR',
+axs[1,1].scatter(extensions, global_p5, marker = '_', label='5th/95th percentile',
                c='gray', s=10)
 
-axs[1,1].scatter(extensions, global_p95, marker = '_', label='RNR',
+axs[1,1].scatter(extensions, global_p95, marker = '_',
                c='gray', s=10)
 axs[1,1].set_xlabel('Neighbour count')
 
 axs[1,1].grid()
 
 axs[1,1].set_axisbelow(True)
-axs[1,1].title.set_text('KNeighborsREgressor, weights=uniform')
+axs[1,1].title.set_text('KNeighborsRegressor,\nweights=uniform')
+axs[1,1].axhline(y=0.199, xmin=0, xmax=1, color='black', linewidth=1,
+                 label='no resampling,\naverage')
+axs[1,1].axhline(y=0.101, xmin=0, xmax=1, color='black', linewidth=1,
+                 label='no resampling,\n5th/95th percentile', linestyle = '--')
+axs[1,1].axhline(y=0.35, xmin=0, xmax=1, color='black', linewidth=1, linestyle = '--')
+#axs[1,1].legend()
 
-from statsmodels.nonparametric.smoothers_lowess import lowess
+# from statsmodels.nonparametric.smoothers_lowess import lowess
 
 
 
-carlo=2*sto
-global_p95 = []
-global_p5 = []
-extensions = np.linspace(0.01,0.4,100)
-global_results = []
-for i in extensions:
-    print("#", end="")
-    local_results = []
-    for j in range(carlo):
-        x = np.random.uniform(start, stop, samples)
-        x = np.sort(x)
-        y = np.sin(x)
+# carlo=2*sto
+# global_p95 = []
+# global_p5 = []
+# extensions = np.linspace(0.01,0.4,100)
+# global_results = []
+# for i in extensions:
+#     print("#", end="")
+#     local_results = []
+#     for j in range(carlo):
+#         x = np.random.uniform(start, stop, samples)
+#         x = np.sort(x)
+#         y = np.sin(x)
         
-        max_step = np.max(np.diff(x))
-        extension = i
+#         max_step = np.max(np.diff(x))
+#         extension = i
         
     
-        x_uniform = np.linspace(start, stop , samples)
+#         x_uniform = np.linspace(start, stop , samples)
         
-        y_reg = lowess(y, x, frac=i, xvals=x_uniform)
+#         y_reg = lowess(y, x, frac=i, xvals=x_uniform)
         
         
-        diff = y[10:-10]-y_reg[10:-10]
+#         diff = y[10:-10]-y_reg[10:-10]
 
-        result = np.mean(np.abs(diff))
+#         result = np.mean(np.abs(diff))
         
-        local_results.append(result)
+#         local_results.append(result)
 
         
-    global_results.append(np.mean(local_results))
-    global_p5.append(np.percentile(local_results,5))
-    global_p95.append(np.percentile(local_results,95))
+#     global_results.append(np.mean(local_results))
+#     global_p5.append(np.percentile(local_results,5))
+#     global_p95.append(np.percentile(local_results,95))
     
     
-axs[2,0].scatter(extensions, global_results, marker = '.', label='mean',
-               c='black', s=10)
+# axs[2,0].scatter(extensions, global_results, marker = '.', label='mean',
+#                c='black', s=10)
 
-axs[2,0].scatter(extensions, global_p5, marker = '_', label='5th/95th\npercentile',
-               c='gray', s=10)
+# axs[2,0].scatter(extensions, global_p5, marker = '_', label='5th/95th\npercentile',
+#                c='gray', s=10)
 
-axs[2,0].scatter(extensions, global_p95, marker = '_', 
-               c='gray', s=10)
-axs[2,0].set_xlabel('Data fraction')
+# axs[2,0].scatter(extensions, global_p95, marker = '_', 
+#                c='gray', s=10)
+# axs[2,0].set_xlabel('Data fraction')
 
-axs[2,0].set_ylabel('Mean Absolute Error')
-axs[2,0].grid()
-axs[2,0].set_axisbelow(True)
-axs[2,0].title.set_text('LOWESS')
+# axs[2,0].set_ylabel('Mean Absolute Error')
+# axs[2,0].grid()
+# axs[2,0].set_axisbelow(True)
+# axs[2,0].title.set_text('LOWESS')
 
 
 
-axs[2,1].xaxis.set_visible(False)
-axs[2,1].yaxis.set_visible(False)
-axs[2,1].axis('off')
-axs[2,1].scatter([], [], marker = '.', label='mean',
-               c='black', s=10)
+# axs[2,1].xaxis.set_visible(False)
+# axs[2,1].yaxis.set_visible(False)
+# axs[2,1].axis('off')
+# axs[2,1].scatter([], [], marker = '.', label='mean',
+#                c='black', s=10)
 
-axs[2,1].scatter([], [], marker = '_', label='5th/95th\npercentile',
-               c='gray', s=10)
+# axs[2,1].scatter([], [], marker = '_', label='5th/95th\npercentile',
+#                c='gray', s=10)
 
-axs[2,1].scatter([], [], marker = '_', 
-               c='gray', s=10)
-axs[2,1].legend(loc='center')
+# axs[2,1].scatter([], [], marker = '_', 
+#                c='gray', s=10)
+# axs[2,1].legend(loc='center')
 plt.tight_layout()
 plt.savefig('resampling.pdf')
 plt.show()
@@ -356,3 +379,46 @@ plt.xlabel('x\n[index]')
 plt.ylabel('y=sin(x)')
 plt.tight_layout()
 plt.savefig('resampling_theory.pdf')
+
+#%%
+
+local_results = []
+for j in range(1000):
+    start = 0
+    stop = 10
+    samples = 100
+    x = np.random.uniform(start, stop, samples)
+    x = np.sort(x)
+    y = np.sin(x)
+    
+    
+    max_step = np.max(np.diff(x))
+
+
+    
+    x_uniform = np.linspace(start, stop, samples)
+    
+    y_uniform = np.sin(x_uniform)
+    
+    
+    reg = KNeighborsRegressor(n_neighbors = 3,
+                              weights='uniform')
+    reg.fit(x.reshape(-1,1),y)
+    
+    x_uniform = np.linspace(start, stop , samples)
+    
+    y_reg = reg.predict(x_uniform.reshape(-1,1))
+    
+    
+    # plt.plot(x_uniform, y_uniform)
+    # plt.plot(x_uniform, y)
+    # plt.plot(x_uniform, y_reg)
+    # plt.show()
+    diff = y - y_uniform
+    result = np.mean(np.abs(diff))
+    
+    local_results.append(result)
+    
+print(f'Average: {np.average(local_results)}')
+print(f'5th percentile {np.percentile(local_results,5)}')
+print(f'95th percentile {np.percentile(local_results,95)}')
